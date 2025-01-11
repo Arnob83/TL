b179cf51600e45db8dd60c66663f9fe218de99f2
Arnob Dey <85866452+Arnob83@users.noreply.github.com>
2025-01-11 17:17:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def prediction(Credit_History, Education_1, ApplicantIncome, CoapplicantIncome, 
 
 # Explanation function
 def explain_prediction(input_data, final_result):
-    explainer = shap.TreeExplainer(classifier)
+    explainer = shap.Explainer(classifier)
     shap_values = explainer.shap_values(input_data)
     shap_values_for_input = shap_values[0]
 
